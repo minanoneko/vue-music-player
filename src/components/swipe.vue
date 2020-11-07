716e59120dbeb0a6855d939bf50761e41af7c7d6
wt <1576782213@qq.com>
2020-11-07 18:56:17 +0800

    播放组件

---
+++ src/components/swipe.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <swiper :options="swiperOption" ref="mySwiper">
+            <swiper-slide>
+                <div class="disc-cover" :class="{'play-ani':isPlay}">
+                    <div class="disc">
+                        <img :src="this.cover.cover" alt="">
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+    import 'swiper/swiper-bundle.css'
+
+    export default {
+        name: "swipe",
+        props:['cover','isPlay'],
+        components:{
+            Swiper,
+            SwiperSlide
+        },
+        data(){
+            return{
+                swiperOptions:{
+                    autoplay:false,
+                },
+            }
+        },
+        computed:{
+
+    },
+        methods:{
+
+        },
+        mounted(){
+
+        }
+
+    }
+</script>
+
+<style lang="scss"  scoped>
+    .swiper-slide{
+        width: 100%;
+        .disc-cover{
+            width: 100%;
+            margin-top: 1rem;
+           .disc{
+               width: 5rem;
+               height: 5rem;
+               border-radius: 50%;
+               overflow: hidden;
+                margin: 0 auto;
+               box-sizing: border-box;
+               border: 0.23rem solid #bdb4b4;
+               img{
+                   width: 100%;
+                   height: 100%;
+               }
+           }
+        }
+        .play-ani{
+            animation: play 5s infinite linear;
+        }
+        @keyframes play {
+            from{
+                transform: rotate(0);
+            }
+            to{
+                transform: rotate(360deg);
+            }
+        }
+    }
+</style>
---
+++ src/utils/resetRem.js
@@ -0,0 +1,8 @@
+export function resetRem(){
+  //有些浏览器documentElement获取不到,那就使用后面的body
+  let htmlWidth=document.documentElement.clientWidth || document.body.clientWidth;
+
+  let htmlDom=document.getElementsByTagName("html")[0];
+
+  htmlDom.style.fontSize=htmlWidth/750*100+'px';
+}
